142b83d4552dfbe0e6dc21e6c46af0d297ec47ef
Michael Smith <89611865061@ya.ru>
2017-05-02 02:33:48 +0300

    create-menu,change-border-radius

--- source/js/app.js
+++ source/js/app.js
@@ -1,7 +1,33 @@
-(function() {
-  'use strict';
+// menu_______________________________________
+var Hamburger = (function () {
+    var
+        hamburger = $('.menu__hamburger__link'),
+        navContainer = $('.js-navigation'),
+        navContent = $('.main-menu');
 
-  setTimeout(function() {
-    document.querySelector('.greating_picture').classList.add('m--show');
-  }, 1000);
-})();
+    return {
+        init: function () {
+            hamburger.on('click', function (e) {
+                e.preventDefault();
+
+                var _this = $(this);
+
+                _this.toggleClass('on');
+                $('body').toggleClass('active');
+                setTimeout(function () {
+                    navContent.toggleClass('active');
+                }, 500);
+                navContainer.toggleClass('active')
+            });
+        }
+    }
+}());
+
+$(function () {
+    if ($('#hamburger').length) {
+        Hamburger.init();
+    }
+    if ($('#arrow').length) {
+        Arrow.init();
+    }
+});
