9d516ee9259f36207ae3e032569f1585bc932085
Michael Smith <89611865061@ya.ru>
2017-05-08 12:19:48 +0300

    commit

--- source/js/app.js
+++ source/js/app.js
@@ -130,8 +130,112 @@ var ParallaxMouse = (function () {
     }
 }());
 
+var ParallaxScroll = (function () {
+    return {
+        init: function () {
+            window.onscroll = function () {
+                var parallax = (function () {
+                    var
+                        bg = document.querySelector('.js-parallax-bg'),
+                        title = document.querySelector('.js-parallax-title'),
+                        user = document.querySelector('.js-parallax-user');
+
+                    return {
+                        move: function (block, windowScroll, strafeAmount) {
+                            var
+                                strafe = windowScroll/-strafeAmount + '%',
+                                style = block.style,
+                                transformString = 'translate3d(0,'+ strafe +', 0)'
+                            style.top = strafe;
+                            style.transform = transformString;
+                            style.webkitTransform = transformString;
+                        },
+                        init: function (wScroll) {
+                            this.move(bg, wScroll, 45, 0);
+                            this.move(title, wScroll, 15, 50);
+                            this.move(user, wScroll, 5, 50);
+                        }
+                    }
+                }());
+                var wScroll = window.pageYOffset;
+                parallax.init(wScroll);
+            };
+        }
+    }
+}());
+
 $(function () {
     if ($('#parallax-mouse').length){
         ParallaxMouse.init();
     }
+
+    if ($('#parallax-Scroll').length) {
+        ParallaxScroll.init();
+    }
 })
+
+// preloader___________
+
+var preloader = (function () {
+    var
+        preloader = $('.preloader'),
+        persentsTotal = 0,
+        cardAnimate = $('.usercart');
+    var imgPath = $('*').map(function (ind, element) {
+
+        var
+            background = $(element).css('background-image'),
+            path = '';
+        var isImg = $(element).is('img');
+
+        if (background != 'none') {
+            path = background.replace('url("', '').replace('")','')
+        }
+
+        if (isImg) {
+            path = $(element).attr('src')
+        }
+
+        if (path) return path;
+    });
+
+    var setPersents = function (total, current) {
+
+        var persents = Math.ceil(current / total *100);
+        $('.js_percents').text(persents + '%');
+
+        if (persents >= 100) {
+            preloader.fadeOut();
+            cardAnimate.addClass('active');
+        }
+    };
+
+    var loadImages = function (images) {
+        if (!images.length) preloader.fadeOut();
+
+        images.forEach(function (img, i, images) {
+            var fakeImages = $('<img>', {
+                attr: {
+                    src: img
+                }
+            });
+
+            fakeImages.on('load error', function () {
+                persentsTotal++;
+                setPersents(images.length, persentsTotal);
+            })
+        });
+
+    };
+
+    return {
+        init: function () {
+            var imgs = imgPath.toArray();
+            loadImages(imgs);
+        }
+    }
+}());
+
+$(function () {
+    preloader.init();
+});
