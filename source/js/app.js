ef99fa1f1cc4e9ae6d9f680333eb72cbbf9a5919
Michael Smith <89611865061@ya.ru>
2017-05-08 16:15:47 +0300

    blog repair

--- source/js/app.js
+++ source/js/app.js
@@ -239,3 +239,102 @@ var preloader = (function () {
 $(function () {
     preloader.init();
 });
+
+//Menu__Blog______________
+var scrollMenu = (function () {
+    var $news = $('.blog__contain__list__link'),
+        $item = $('.blog__menu__list__link'),
+        $wrapMenu = $('.blog__menu'),
+        body = document.body,
+        isPositionArticle = [],
+        offsetHeight = 200,
+
+        positionArticle = function (element) {
+            var len = element.length;
+            for (var i = 0; i < len; i++) {
+                isPositionArticle[i] = {};
+                isPositionArticle[i].top = element
+                        .eq(i)
+                        .offset()
+                        .top - offsetHeight;
+                isPositionArticle[i].bottom = isPositionArticle[i].top + element
+                        .eq(i)
+                        .innerHeight();
+            }
+        },
+
+        scrollPageFixMenu = function () {
+            var scroll = window.pageYOffset;
+            if (scroll < $news.offset().top) {
+                $wrapMenu.removeClass('fixed');
+            } else {
+                $wrapMenu.addClass('fixed');
+            }
+        },
+
+        scrollPage = function () {
+            var scroll = window.pageYOffset;
+            for (var i = 0; i < isPositionArticle.length; i++) {
+                if (scroll >= isPositionArticle[i].top && scroll <= isPositionArticle[i].bottom) {
+                    $('.blog__menu__list__link_phone')
+                        .eq(i)
+                        .addClass('link_active')
+                        .siblings()
+                        .removeClass('link_active');
+                    $item
+                        .eq(i)
+                        .addClass('link_active')
+                        .siblings()
+                        .removeClass('link_active');
+                }
+            }
+        },
+
+        clickOnMenu = function (e) {
+            var index = $(e.target).index();
+            var sectionOffset = $news
+                .eq(index)
+                .offset()
+                .top;
+            $(document).off('scroll', scrollPage);
+            $('body, html').animate({
+                'scrollTop': sectionOffset
+            }, function () {
+                $(e.target)
+                    .addClass('link_active')
+                    .siblings()
+                    .removeClass('link_active');
+                $(document).on('scroll', scrollPage);
+            });
+        },
+
+        addListener = function () {
+            $('.blog__menu__list').on('click', clickOnMenu);
+
+            $(document).on('scroll', scrollPage);
+            $(document).on('scroll', scrollPageFixMenu);
+
+            $(window).on('load', function (e) {
+                positionArticle($news);
+            });
+
+            $(window).on('resize', function (e) {
+                positionArticle($news);
+            });
+
+            $('.blog_phone__menu__js_disk').on('click', function (e) {
+                e.preventDefault();
+                $(this).parents('.blog__aside_phone').toggleClass('blocked');
+            });
+        };
+
+    return {
+        init: addListener
+    }
+}());
+
+$(function () {
+    if ($('#blog').length) {
+        scrollMenu.init();
+    }
+});
